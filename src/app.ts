93dcd44534192f4b27b788225282bda2676d83e0
emiliozallocco1 <ezallocco@frro.utn.edu.ar>
2025-12-09 19:48:22 -0300

    configuraciones para el deploy en render

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,44 @@
 import 'reflect-metadata'
 import express from 'express'
 import cors from 'cors'
-// import { characterRouter } from './character/character.routes.js'
-// import { characterClassRouter } from './character/characterClass.routes.js'
-// import { itemRouter } from './character/item.routes.js'
 import { orm, syncSchema } from './shared/db/orm.js'
 import { RequestContext } from '@mikro-orm/core'
 import { especialidadRouter } from './especialidad/especialidad.routes.js'
 import { medicoRouter } from './medico/medico.routes.js'
 import { turnoRouter } from './turno/turno.routes.js'
 import { pacienteRouter } from './paciente/paciente.routes.js'
-import { obraSocialRouter} from './obraSocial/obraSocial.routes.js'
-
-
-
+import { obraSocialRouter } from './obraSocial/obraSocial.routes.js'
 
 const app = express()
+
 app.use(express.json())
 app.use(cors())
 
-//luego de los middlewares base
+// Contexto de MikroORM por request
 app.use((req, res, next) => {
   RequestContext.create(orm.em, next)
 })
-//antes de las rutas y middlewares de negocio
 
-
-//==================================================================
-// app.use('/api/characters/classes', characterClassRouter)
-// app.use('/api/characters', characterRouter)
-// app.use('/api/items', itemRouter)
-
-app.use('/api/especialidades/',especialidadRouter)
+// Rutas
+app.use('/api/especialidades/', especialidadRouter)
 app.use('/api/medicos/', medicoRouter)
 app.use('/api/turnos/', turnoRouter)
 app.use('/api/pacientes/', pacienteRouter)
 app.use('/api/obras-sociales/', obraSocialRouter)
 
-//===================================================================
-
+// 404
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resource not found' })
 })
 
-await syncSchema() //never in production
+//  NUNCA correr syncSchema en producción
+if (process.env.NODE_ENV !== 'production') {
+  await syncSchema()
+}
 
-app.listen(3000, () => {
-  console.log('Server runnning on http://localhost:3000/')
+// Puerto dinámico para Render
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`)
 })
